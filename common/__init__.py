88c38d69e128b9faf363a687f26de36504f751ff
shiraeeshi <shiraeeshi@mail.ru>
2020-10-09 07:27:59 +0600

    import Gdk in common

--- common/__init__.py
+++ common/__init__.py
@@ -6,5 +6,5 @@ gi.require_version('Gtk', '3.0')
 gi.require_version('WebKit2', '4.0')
 gi.require_version('WebKit2WebExtension', '4.0')
 
-from gi.repository import Gtk as gtk, AppIndicator3 as appindicator, WebKit2
+from gi.repository import Gtk as gtk, Gdk, AppIndicator3 as appindicator, WebKit2
 
