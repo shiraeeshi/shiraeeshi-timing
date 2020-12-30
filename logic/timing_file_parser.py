7145d302598622baf645a2e3c7e8036978e03625
shiraeeshi <shiraeeshi@mail.ru>
2020-12-31 02:48:23 +0600

    timing_file_parser: add pattern for single-line date and time

--- logic/timing_file_parser.py
+++ logic/timing_file_parser.py
@@ -23,6 +23,7 @@ def parse_timing_file_lines(lines):
 
     pattern_date = "([0-9]{2})\.([0-9]{2})\.([0-9]{2,4})"
     pattern_timing = "([0-9]{2}):([0-9]{2}) - ([0-9]{2}):([0-9]{2}) [- *] (\(([0-9]+) m\) )?(.*)"
+    pattern_date_and_timing = pattern_date + " " + pattern_timing
 
     for line in lines:
         match = re.match(pattern_date, line)
@@ -75,6 +76,58 @@ def parse_timing_file_lines(lines):
                 "minutes": minutes,
                 "name": name
                 })
+        match = re.match(pattern_date_and_timing, line)
+        if match is not None:
+            day_of_month = match.group(1)
+            month = match.group(2)
+            year = match.group(3)
+            dict_date = {
+                    "day_of_month": day_of_month,
+                    "month": month,
+                    "year": year
+                    }
+            #print("day: " + str(dict_date))
+            year = int(year)
+            if year < 100:
+                year += 2000
+            dt = [int(day_of_month), int(month), year]
+            if current_day is not None and dt == current_day:
+                pass
+            else:
+                current_day = dt
+                current_day_timings = []
+                result.append({"date": current_day, "timings": current_day_timings})
+
+            from_hour = match.group(4)
+            from_minute = match.group(5)
+            to_hour = match.group(6)
+            to_minute = match.group(7)
+            minutes = match.group(9)
+            name = match.group(10)
+            timing = {
+                    "from_hour": from_hour,
+                    "from_minute": from_minute,
+                    "to_hour": to_hour,
+                    "to_minute": to_minute,
+                    "minutes": minutes,
+                    "name": name
+                    }
+            #print("timing: " + str(timing))
+            from_hour = int(from_hour)
+            from_minute = int(from_minute)
+            to_hour = int(to_hour)
+            to_minute = int(to_minute)
+            if minutes is None:
+                minutes = compute_minutes(from_hour, from_minute, to_hour, to_minute)
+            else:
+                minutes = int(minutes)
+
+            current_day_timings.append({
+                "from": [from_hour, from_minute],
+                "to": [to_hour, to_minute],
+                "minutes": minutes,
+                "name": name
+                })
         if line == "-":
             #print("line is -")
             pass
@@ -86,3 +139,4 @@ def compute_minutes(from_hour, from_minute, to_hour, to_minute):
     hour_diff = to_hour - from_hour
     minutes = to_minute - from_minute + 60*hour_diff
     return minutes
+
