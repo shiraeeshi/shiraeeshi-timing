abedcf7c6b2cf230601d852211ab29f592e86d57
shiraeeshi <shiraeeshi@mail.ru>
2022-05-29 04:49:34 +0600

    timing_index_manager: save timing-name-to-index-names association in tmp folder, also save and read last modified times of timings, then decide whether to recreate index or not

--- logic/timing_index_manager.py
+++ logic/timing_index_manager.py
@@ -15,56 +15,118 @@ def create_index():
     if not os.path.exists(tmp_dir):
         os.makedirs(tmp_dir)
 
+    file_index_names = os.path.join(tmp_dir, "filenames_of_indexes")
+
     timing2indexFilename = {}
+
+    if os.path.exists(file_index_names):
+        pattern_names = "index-name: ([a-z0-9-]+), timing-name: (.+)$"
+        with open(file_index_names) as f:
+            lines = f.readlines()
+            for line in lines:
+                match = re.match(pattern_names, line)
+                if match is None:
+                    continue
+                index_name = match.group(1)
+                timing_name = match.group(2)
+                timing2indexFilename[timing_name] = index_name
 
     with open(config_file) as f:
         contents = f.read().rstrip()
         #print("config contents: {}".format(contents))
         config = json2config(contents)
 
-        pattern_date = "([0-9]{2})\.([0-9]{2})\.([0-9]{2,4})"
-        pattern_timing = "([0-9]{2}):([0-9]{2}) - ([0-9]{2}):([0-9]{2})( [- *] \(([0-9]+) m\) )?"
-        pattern_date_and_timing = pattern_date + " " + pattern_timing
-
-
         for timing in config.timings:
             timing_name = timing.get("name")
-            filepath = os.path.expanduser(timing.get("filepath"))
-            timing_uuid = uuid.uuid4()
-            timing2indexFilename[timing_name] = str(timing_uuid)
-            tmp_index_filepath = os.path.join(tmp_dir, str(timing_uuid))
-            frmt = timing.get("format")
-            print("timing: {}".format(timing_name))
-            with open(filepath, encoding='utf-8') as f:
-                with open(tmp_index_filepath, 'a', encoding='utf-8') as f_out:
-                    prev_index_entry = None
-                    while True:
-                        offset = f.tell()
-                        line = f.readline()
+            index_name = None
+            if timing_name in timing2indexFilename:
+                index_name = timing2indexFilename[timing_name]
+                timing_filepath = os.path.expanduser(timing.get("filepath"))
+                tmp_index_filepath = os.path.join(tmp_dir, index_name)
+                if os.path.isfile(tmp_index_filepath):
+                    _refresh_index(timing, index_name, tmp_dir)
+                else:
+                    _create_index_for_timing(timing, index_name, tmp_dir)
+            else:
+                timing_uuid = uuid.uuid4()
+                index_name = str(timing_uuid)
+                timing2indexFilename[timing_name] = index_name
+                _create_index_for_timing(timing, index_name, tmp_dir)
 
-                        if line == "":
-                            if prev_index_entry != None:
-                                f_out.write("date: {}, offset_from: {}, offset_to: {}\n".format(prev_index_entry["date"], prev_index_entry["offset_from"], offset))
-                            break
-
-                        match = re.match(pattern_date_and_timing, line)
-                        if match is None:
-                            continue
-                        day_of_month = match.group(1)
-                        month = match.group(2)
-                        year = match.group(3)
-                        year = int(year)
-                        if year < 100:
-                            year += 2000
-                        a_date = "{}.{}.{}".format(day_of_month, month, year)
-                        if prev_index_entry == None:
-                            prev_index_entry = {"date": a_date, "offset_from": offset}
-                            continue
-                        if a_date != prev_index_entry["date"]:
-                            f_out.write("date: {}, offset_from: {}, offset_to: {}\n".format(prev_index_entry["date"], prev_index_entry["offset_from"], offset))
-                            prev_index_entry = {"date": a_date, "offset_from": offset}
+    with open(file_index_names, 'w') as f:
+        for timing_name, index_name in timing2indexFilename.items():
+            f.write("index-name: {}, timing-name: {}\n".format(index_name, timing_name))
 
     return timing2indexFilename
+
+def _refresh_index(timing, index_name, tmp_dir):
+    timing_name = timing.get("name")
+    timing_filepath = os.path.expanduser(timing.get("filepath"))
+    timing_last_modified = os.path.getmtime(timing_filepath)
+
+    index_last_modified_filepath = os.path.join(tmp_dir, index_name + ".last_modified")
+    if os.path.isfile(index_last_modified_filepath):
+        with open(index_last_modified_filepath) as f_ilm:
+            index_last_modified = None
+            ilm_str = ""
+            try:
+                ilm_str = f_ilm.read()
+                index_last_modified = float(ilm_str)
+            except:
+                print("error reading index_last_modified for timing {}, value: {}".format(timing_name, ilm_str))
+                index_last_modified = None
+
+            if index_last_modified == timing_last_modified:
+                print("index_last_modified is the same as timing_last_modified, skipping recreating index for timing {}".format(timing_name))
+                pass # do nothing, no need to recreate the index
+            elif index_last_modified is None:
+                print("couldn't read index_last_modified for timing {}, recreating index".format(timing_name))
+                tmp_index_filepath = os.path.join(tmp_dir, index_name)
+                os.remove(tmp_index_filepath) # wipe out the old contents
+                _create_index_for_timing(timing, index_name, tmp_dir)
+            else:
+                tmp_index_filepath = os.path.join(tmp_dir, index_name)
+                os.remove(tmp_index_filepath) # wipe out the old contents TODO implement index refresh
+                _create_index_for_timing(timing, index_name, tmp_dir)
+
+def _create_index_for_timing(timing, index_name, tmp_dir):
+    timing_name = timing.get("name")
+    print("timing: {}".format(timing_name))
+    timing_filepath = os.path.expanduser(timing.get("filepath"))
+    tmp_index_filepath = os.path.join(tmp_dir, index_name)
+    with open(timing_filepath, encoding='utf-8') as f:
+        with open(tmp_index_filepath, 'a', encoding='utf-8') as f_out:
+            prev_index_entry = None
+            while True:
+                offset = f.tell()
+                line = f.readline()
+
+                if line == "":
+                    if prev_index_entry != None:
+                        f_out.write("date: {}, offset_from: {}, offset_to: {}\n".format(prev_index_entry["date"], prev_index_entry["offset_from"], offset))
+                    break
+
+                match = re.match(pattern_date_and_timing, line)
+                if match is None:
+                    continue
+                day_of_month = match.group(1)
+                month = match.group(2)
+                year = match.group(3)
+                year = int(year)
+                if year < 100:
+                    year += 2000
+                a_date = "{}.{}.{}".format(day_of_month, month, year)
+                if prev_index_entry == None:
+                    prev_index_entry = {"date": a_date, "offset_from": offset}
+                    continue
+                if a_date != prev_index_entry["date"]:
+                    f_out.write("date: {}, offset_from: {}, offset_to: {}\n".format(prev_index_entry["date"], prev_index_entry["offset_from"], offset))
+                    prev_index_entry = {"date": a_date, "offset_from": offset}
+
+    index_last_modified_filepath = os.path.join(tmp_dir, index_name + ".last_modified")
+    timing_last_modified = os.path.getmtime(timing_filepath)
+    with open(index_last_modified_filepath, 'w') as f_ilm:
+        f_ilm.write("{}".format(timing_last_modified))
 
 def read_index(indexFilename):
     filepath = os.path.join(ROOT_DIR, "tmp", indexFilename)
@@ -91,6 +153,10 @@ def read_index(indexFilename):
 
 
     return offsets_by_date
+
+pattern_date = "([0-9]{2})\.([0-9]{2})\.([0-9]{2,4})"
+pattern_timing = "([0-9]{2}):([0-9]{2}) - ([0-9]{2}):([0-9]{2})( [- *] \(([0-9]+) m\) )?"
+pattern_date_and_timing = pattern_date + " " + pattern_timing
 
 def main():
     create_index()
