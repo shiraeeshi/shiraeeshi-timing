1dba142c5116a35fe341f09adf1c7372a82d17b1
shiraeeshi <shiraeeshi@mail.ru>
2023-03-18 01:33:10 +0600

    require version 0.1 of AppIndicator3

--- common/__init__.py
+++ common/__init__.py
@@ -5,6 +5,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('WebKit2', '4.0')
 gi.require_version('WebKit2WebExtension', '4.0')
+gi.require_version('AppIndicator3', '0.1')
 
 from gi.repository import Gtk as gtk, Gdk, AppIndicator3 as appindicator, WebKit2
 
