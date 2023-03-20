d07295545d97c9cf5b2f674b4fc94e2137209756
shiraeeshi <shiraeeshi@mail.ru>
2023-03-20 07:55:47 +0600

    port to electron: delete some old python files

--- models/indic_env.py
+++
@@ -1,7 +0,0 @@
-from logic.timing_timer_manager import TimingTimerManager
-
-class IndicEnv(object):
-    def __init__(self, indicator):
-        self.indicator = indicator
-        self.timing_manager = TimingTimerManager()
-
--- models/timing_timer_info.py
+++
@@ -1,6 +0,0 @@
-
-class TimingTimerInfo(object):
-    def __init__(self, name, started_at):
-        self.name = name
-        self.started_at = started_at
-
