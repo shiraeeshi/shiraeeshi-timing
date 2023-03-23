e0a441c5f71d6c1c13f1f3ca1d727d02872ecc52
shiraeeshi <shiraeeshi@mail.ru>
2023-03-23 06:34:35 +0600

    src-frontend divide to webpack bundles: rm *.my.js files

--- src-frontend/notebook/notebook.my.js
+++
@@ -1,5 +0,0 @@
-let my = {
-  processesForest: null
-};
-
-
--- src-frontend/timings_history/latest.my.js
+++
@@ -1,9 +0,0 @@
-let my = {
-  timings: null,
-  // imageInfo: { // imageInfo = new ImageInfo()
-  //   minutesRange: 0,
-  //   minutesMaxDiff: 0
-  // },
-  dayOffset: 0
-};
-
